12c9fb16b94c8c4554aef1b9ea81c7a343e4e07a
Beqa Gozalishvili <beqaprogger@gmail.com>
2021-01-19 19:39:37 +0400

    update version and copyright

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GozalMusicMini")]
-[assembly: AssemblyDescription("listen music on vk.com")]
+[assembly: AssemblyDescription("Listen music on vk.com")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("gozaltech")]
+[assembly: AssemblyCompany("Gozaltech")]
 [assembly: AssemblyProduct("GozalMusicMini")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
